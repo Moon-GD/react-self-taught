1be52662d82891a3788bdb3f5e6d5b4646f84127
Moon-gd <king4mun@gmail.com>
2022-10-18 12:10:19 +0900

    Self-Taught ( React Plug-In )

---
+++ src/study/2022_1018 ( React Plug-In )/dir_eslint/test_eslint.js
@@ -0,0 +1,5 @@
+// eslint - "semi" 오류 발생
+console.log("Eslint - semi 오류 발생시키기");
+
+// eslint - "quotes" 오류 발생
+console.log("Eslint - quotes 오류 발생시키기");
---
+++ src/study/2022_1018 ( React Plug-In )/dir_prettier/test_prettier.js
@@ -0,0 +1,6 @@
+// Preference - Default Formatter를 prettier로 지정
+// Auto save format, Paste save format 모두 true로 설정
+
+// 파일이 저장되면 prettier에서 권장하는 스타일링이 적용된 코드로 바뀜
+// ex) String with single quote -> String with Double quotes
+console.log("작은 따옴표로 저장해도 큰 따옴표로 바뀜");
